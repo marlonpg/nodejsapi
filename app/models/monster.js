381d2541765d3c370f903fe250e84df9e99eec2b
Vinicius Moraes <vmoraes.info@gmail.com>
2017-02-08 23:16:46 -0200

    updating monster

--- app/models/monster.js
+++ app/models/monster.js
@@ -2,7 +2,20 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var MonsterSchema   = new Schema({
-    name: String
+    name: String,
+	level: Number,
+	hp: Number,
+	attack: Number,
+	defense: Number,
+	magicAttack: Number,
+	magicDefense: Number,
+	speed: Number,
+	type: String,
+	criticalChance: Number,
+	criticalDamage: Number,
+	evasion: Number,
+	accuracy: Number,
+	isEnemy: Boolean
 });
 
 module.exports = mongoose.model('Monster', MonsterSchema);
