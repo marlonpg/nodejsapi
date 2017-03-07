4c3e029164fc01be95044ebfe61ac796a5238ead
Vinicius Moraes <vmoraes.info@gmail.com>
2017-03-07 01:47:22 -0300

    updating node project with page layout and saving tpl_monster

---
+++ app/models/tpl_monster.js
@@ -0,0 +1,25 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var TPL_MonsterSchema   = new Schema({
+    Name: String,
+	Level: Number,
+	Hp: Number,
+	Attack: Number,
+	Defense: Number,
+	MagicAttack: Number,
+	MagicDefense: Number,
+	Speed: Number,
+	Type: String,
+	CriticalChance: Number,
+	CriticalDamage: Number,
+	Evasion: Number,
+	Accuracy: Number,
+	IsEnemy: Boolean,
+	SpriteName:String,
+	skills: []
+});
+
+
+
+module.exports = mongoose.model('TPL_Monster', TPL_MonsterSchema);
